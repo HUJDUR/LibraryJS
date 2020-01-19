ec49c63a6dac547ca8458b0de25575c2c03442c8
Nadir Hujdur <nadir.hujdur2002@gmail.com>
2020-01-19 14:31:53 +0100

    Progress

---
+++ script.js
@@ -0,0 +1,42 @@
+
+let korisnici = [];
+
+function menu() {
+   
+	let unosKorisnika = document.getElementById('unos').value;
+
+    switch(unosKorisnika) {
+        case 1 : kreiranjeRacuna();
+				break;
+			case 2 : kreiranjeKnjige();
+				break;
+			case 3 : podizanjeKnjige();
+				break;
+			case 4 : vracanjeKnjige();
+				break;
+			case 5 : ispisivanjeDetalja();
+				break;
+    }
+
+}
+
+function kreiranjeRacuna() {
+    
+}
+
+function kreiranjeKnjige() {
+
+}
+
+function podizanjeKnjige() {
+
+}
+
+function vracanjeKnjige() {
+
+}
+
+function ispisivanjeDetalja() {
+
+}
+
