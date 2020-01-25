cd9b659ab87558b8f4985456216a8d481feb94f9
Nadir Hujdur <nadir.hujdur2002@gmail.com>
2020-01-25 20:01:14 +0100

    Progress

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let korisnici = [];
+let korisnici = new Array();
+let knjige = new Array();
+window.onload = izborniMenu();
 
 function izborniMenu() {
 	document.getElementById('naslov').innerHTML = `Koja operacija vam je potrebna?
 	<br>1. Kreiranje racuna   <br>2. Kreiranje knjige   <br>3. Podizanje knjige
 	<br>4. Vracanje knjige   <br>5. Ispisivanje detalja postojecih racuna`;
 }
-
-window.onload = izborniMenu();
 
 function pregledUnosa() {
 
@@ -15,7 +15,29 @@ function pregledUnosa() {
 	switch(unosKorisnika) {
 		case '1' : kreiranjeRacuna();
 		break;
+		case '2' : kreiranjeKnjige();
+		break;
 	}
+}
+
+function kreiranjeKnjige() {
+	
+	let imeKnjige = prompt('Unesite ime knjige:', '');
+	let brojKnjige = prompt('Unesite broj knjige' ,'');
+	let status = false;
+
+	if (provjeraKnjige(brojKnjige)) {
+		alert('Uspjesno ste kreirali knjigu!');
+		
+		let knjiga = {
+			imeKnjige,
+			brojKnjige,
+			status
+		};
+
+		knjige.push(knjiga);
+	}
+
 }
 
 function kreiranjeRacuna() {
@@ -23,19 +45,40 @@ function kreiranjeRacuna() {
 	let imeRacuna = prompt('Unesite vase ime:', '');
 	let brojRacuna = prompt('Unesite broj racuna', '');
 
-	if (provjeraRacuna()) {
+	if (provjeraRacuna(imeRacuna, brojRacuna)) {
 		alert('Racun uspjesno napravljen.');
-	} else {
-		alert('Doslo je do greske!');
-	}
-	
+		
+		let korisnik = {
+			imeRacuna,
+			brojRacuna,
+			knjige : new Array()
+		};
+
+		korisnici.push(korisnik);
+	} 
 }
 
-function provjeraRacuna() {
+function provjeraRacuna(imeRacuna, brojRacuna) {
 	for (let i = 0; i < korisnici.length; i++) {
-
+		if (korisnici[i].brojRacuna == brojRacuna) {
+			alert('Vec postoji racun sa unesenim brojem');
+			return false;
+		} else if (korisnici[i].imeRacuna == imeRacuna) {
+			alert('Vec postoji racun sa unesenim imenom');
+			return false;
+		}
 	}
-	
+	return true;
+}
+
+function provjeraKnjige(brojKnjige) {
+	for (let i = 0; i < knjige.length; i++) {
+		if (knjige[i].brojKnjige == brojKnjige) {
+			alert('Vec postoji knjiga sa unijetim brojem');
+			return false;
+		}
+	}
+	return true;
 }
 
 document.getElementById('button').addEventListener('click', function(){
