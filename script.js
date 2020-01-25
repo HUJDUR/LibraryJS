0a7343f159d7921ea04d91ec7c129dd7e04c47b4
Nadir Hujdur <nadir.hujdur2002@gmail.com>
2020-01-25 13:07:02 +0100

    Progress

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
-
 let korisnici = [];
 
-function menu() {
-   
+function izborniMenu() {
+	document.getElementById('naslov').innerHTML = `Koja operacija vam je potrebna?
+	<br>1. Kreiranje racuna   <br>2. Kreiranje knjige   <br>3. Podizanje knjige
+	<br>4. Vracanje knjige   <br>5. Ispisivanje detalja postojecih racuna`;
+}
+
+window.onload = izborniMenu();
+
+function pregledUnosa() {
+
 	let unosKorisnika = document.getElementById('unos').value;
-
-    switch(unosKorisnika) {
-        case 1 : kreiranjeRacuna();
-				break;
-			case 2 : kreiranjeKnjige();
-				break;
-			case 3 : podizanjeKnjige();
-				break;
-			case 4 : vracanjeKnjige();
-				break;
-			case 5 : ispisivanjeDetalja();
-				break;
-    }
-
+	
+	switch(unosKorisnika) {
+		case '1' : kreiranjeRacuna();
+		break;
+	}
 }
 
 function kreiranjeRacuna() {
-    
+
+	let imeRacuna = prompt('Unesite vase ime:', '');
+	let brojRacuna = prompt('Unesite broj racuna', '');
+
+	if (provjeraRacuna()) {
+		alert('Racun uspjesno napravljen.');
+	} else {
+		alert('Doslo je do greske!');
+	}
+	
 }
 
-function kreiranjeKnjige() {
+function provjeraRacuna() {
+	for (let i = 0; i < korisnici.length; i++) {
 
+	}
+	
 }
 
-function podizanjeKnjige() {
-
-}
-
-function vracanjeKnjige() {
-
-}
-
-function ispisivanjeDetalja() {
-
-}
-
+document.getElementById('button').addEventListener('click', function(){
+	pregledUnosa();
+	document.getElementById('unos').value = '';
+});
